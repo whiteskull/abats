18558eb6fd5e6972b3cc0e1287709662cd4b65c3
whiteskull <serwhite@yandex.ru>
2012-08-29 11:16:28 +0400

    make comment

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <? require_once($_SERVER['DOCUMENT_ROOT'].'/abats/begin.php');
 
     $abats->addContent('pages/home.tpl');
-    
+
+    // commet new
+
     $abats->title .= "Best Android Games";
     
 require_once($_SERVER['DOCUMENT_ROOT'].'/abats/end.php');
